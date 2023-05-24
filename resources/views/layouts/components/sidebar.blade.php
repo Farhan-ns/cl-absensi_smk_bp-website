d780bec2cfd7fc47f8495cd544a8ed9092450ac9
Farhanunnasih <farhanunnasih@gmail.com>
2023-05-24 10:50:09 +0700

    Integrate Sneat Admin Template

---
+++ resources/views/layouts/components/sidebar.blade.php
@@ -0,0 +1,47 @@
+ <!-- Menu -->
+
+ <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+      <a href="index.html" class="app-brand-link">
+          <img src="{{ asset('image/logo-bp.png') }}" style="width: 30%;" alt="Logo BP">
+        <span class="app-brand-text demo menu-text fw-bolder ms-2" style="text-transform: capitalize">
+            Bina Putra
+        </span>
+      </a>
+
+      <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+        <i class="bx bx-chevron-left bx-sm align-middle"></i>
+      </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+      <!-- Dashboard -->
+      <li class="menu-item">
+        <a href="index.html" class="menu-link">
+          <i class="menu-icon tf-icons bx bx-home-circle"></i>
+          <div data-i18n="Analytics">Dashboard</div>
+        </a>
+      </li>
+
+      <li class="menu-item">
+        <a href="index.html" class="menu-link">
+          <i class="menu-icon tf-icons bx bx-home-circle"></i>
+          <div data-i18n="Analytics">Other</div>
+        </a>
+      </li>
+
+      <li class="menu-header small text-uppercase">
+        <span class="menu-header-text">Konfigurasi</span>
+      </li>
+
+      <li class="menu-item">
+        <a href="index.html" class="menu-link">
+          <i class="menu-icon tf-icons bx bx-home-circle"></i>
+          <div data-i18n="Analytics">Other</div>
+        </a>
+      </li>
+    </ul>
+  </aside>
+  <!-- / Menu -->
